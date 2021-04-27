543713bc4def03fa159a358abab26437432fc889
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-04-27 20:05:11 +0200

    Add file structure:

---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>eVivlío</title>
+
+    <!-- Bootstrap CSS and JS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <script src="../assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>

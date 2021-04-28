d33ef26d5a5b12ca54f8a2572b360e3af5a4c9f0
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-04-28 10:26:24 +0200

    Edit footer, header, index.php

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+    // require_once("../config/config.php");
+
+    require_once("../templates/header.php");
+?>
+    <div class="container">Content goes here</div>
+
+<?php 
+    require_once("../templates/footer.php");
+?>
---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col">About Us</div>
+                <div class="col">Contact Us</div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>

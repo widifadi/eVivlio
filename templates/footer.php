5f067dfc04151b6547860b8f819a18ce799228c9
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-05-04 18:10:22 +0200

    Edit footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,19 @@
     <footer class="footer">
         <div class="container">
             <div class="row">
-                <div class="col" id="about-us">About Us</div>
-                <div class="col" id="contact-us">Contact Us</div>
+                <div class="col-6" id="about-us">
+                    <a href="about.php"><div class="subtitle footer-subtitle">About Us</div></a>
+                    <div id="store-desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Fusce convallis id dolor non pretium. Nulla nibh neque, 
+                        fermentum sit amet ipsum a, semper malesuada urna. 
+                        Sed non odio vel tortor tempor hendrerit ut id ipsum. 
+                    </div>
+                </div>
+                <div class="col-6" id="contact-us">
+                    <a href="contact.php"><div class="subtitle footer-subtitle">Contact Us</div></a>
+
+                </div>
             </div>
         </div>
     </footer>

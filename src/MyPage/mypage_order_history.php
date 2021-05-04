159863e417f29b288f0c901b5da3b60335d58088
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-04 16:11:05 +0200

    Add files via upload

---
+++ src/MyPage/mypage_order_history.php
@@ -0,0 +1,41 @@
+<div class="container" id="order-history">
+    <span class="subtitle" style="font-size: 16px;">Order History</span>
+        <div class="row"> 
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+  
+        </div>
+</div>
---
+++ src/MyPage/mypage_wishlist.php
@@ -0,0 +1,45 @@
+<div class="container" id="wishlist">
+    <span class="subtitle" style="font-size: 16px;">My Wishlist</span>
+        <div class="row"> 
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>
+            <div class="col wl-book">
+                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                <br>
+                <div id="book-title">"Title", Author (Year)</div>
+            </div>    
+        </div>
+</div>

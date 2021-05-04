ec222985d094838616a541722c9bddad91dfbb86
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-04 16:10:48 +0200

    Create mypage_details.php

---
+++ src/MyPage/mypage_details.php
@@ -0,0 +1,47 @@
+<div class="container" id="personal-details">
+    <span class="subtitle" style="font-size: 16px;">Personal Details</span>
+    <br>
+    <br>
+      <div class="row">
+         <div class="col-3">
+             <h6 class="mb">Full Name</h6>
+          </div>
+
+          <div class="col-8 text-secondary">
+               Someone
+          </div>
+        </div>
+      <div class="row">
+         <div class="col-3">
+             <h6 class="mb">Username</h6>
+          </div>
+          <div class="col-8 text-secondary">
+               someone_cool
+          </div>
+      </div>
+      <div class="row">
+         <div class="col-3">
+             <h6 class="mb">E-mail</h6>
+          </div>
+          <div class="col-8 text-secondary">
+               someone_cool@gmail.com
+          </div>
+      </div>
+      <div class="row">
+         <div class="col-3">
+             <h6 class="mb">Date of birth</h6>
+          </div>
+          <div class="col-8 text-secondary">
+               22/04/1998
+          </div>
+      </div>
+      <div class="row">
+         <div class="col-3">
+             <h6 class="mb">Contact Number</h6>
+          </div>
+          <div class="col-8 text-secondary">
+               +3000000000
+          </div>
+      </div>
+      <button type="button" class="btn btn-outline-warning">Edit</button>
+</div>

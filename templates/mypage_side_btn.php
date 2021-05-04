2fc0d97b2b8c8ebc480638c6ac3b7fdc3c18a82e
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-04 18:03:33 +0200

    Add files via upload

---
+++ templates/mypage_side_btn.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="col">
+        <div class="row">
+            <a href="edit_details.php">
+                <button class="btn btn-outline-warning btn-mypage-menu">Personal Details</button>
+            </a> 
+        </div>
+        <div class="row">
+            <a href="wishlist.php">
+                <button class="btn btn-outline-warning btn-mypage-menu">My Wishlist</button>
+            </a> 
+        </div>
+        <div class="row">
+            <a href="order_history.php">
+                <button class="btn btn-outline-warning btn-mypage-menu">Order History</button>
+            </a> 
+        </div>
+    </div>
+</div>

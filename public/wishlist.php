e5ce38806513a0d0d5f1c036d056ec6279026b24
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-05 14:57:31 +0200

    Update wishlist.php

--- public/wishlist.php
+++ public/wishlist.php
@@ -5,11 +5,15 @@
 <div class="container">
     <!-- breadcrumbs -->
 </div>
-<div class="row">
+<div class="row" style="margin-top: 100px">
     <div class="col-3">
         <?php require_once("../templates/mypage_side_btn.php"); ?>
     </div>
     <div class="col-8">
+        <div class="container catalog-breadcrumbs">
+                <a href="my_page.php"> My Page </a> > 
+                <a href="wishlist.php"> My Wishlist </a>     
+        </div>
         <?php require_once("../src/Mypage/mypage_wishlist.php"); ?>
     </div>
 </div>

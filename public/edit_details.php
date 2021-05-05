0b101813768411c71eb1dfb67b83df297ec25042
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-05 14:56:12 +0200

    Update edit_details.php

--- public/edit_details.php
+++ public/edit_details.php
@@ -2,14 +2,16 @@
     require_once("../templates/header.php");
 ?>
 
-<div class="container">
-    <!-- breadcrumbs -->
-</div>
-<div class="row">
+
+<div class="row" style="margin-top: 100px">
     <div class="col-3">
         <?php require_once("../templates/mypage_side_btn.php"); ?>
     </div>
     <div class="col-8">
+        <div class="container catalog-breadcrumbs">
+                <a href="my_page.php"> My Page </a> > 
+                <a href="edit_details.php"> Edit Details </a>     
+        </div>
         <?php require_once("../src/Mypage/edit_details_form.php"); ?>
     </div>
 </div>

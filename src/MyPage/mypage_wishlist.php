416a6be74c93bae69688096b446e6743d4d26fce
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-05 15:00:05 +0200

    Update mypage_wishlist.php

--- src/MyPage/mypage_wishlist.php
+++ src/MyPage/mypage_wishlist.php
@@ -1,5 +1,5 @@
-<div class="container" id="wishlist">
-    <span class="subtitle" style="font-size: 16px;">My Wishlist</span>
+<div class="container mt-3" id="wishlist">   
+ <a class="title-link" href="wishlist.php" ><span class="subtitle" style="font-size: 16px;">My Wishlist </span></a>
         <div class="row"> 
             <div class="col wl-book">
                 <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">

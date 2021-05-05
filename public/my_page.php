e5dbb6a24bc2f62bc43c08b8025f49254624b5c4
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-05 14:56:43 +0200

    Update my_page.php

--- public/my_page.php
+++ public/my_page.php
@@ -2,15 +2,15 @@
     require_once("../templates/header.php");
 ?>
 
-<div class="container">
-    <!-- breadcrumbs -->
-</div>
 
-<div class="row">
+<div class="row" style="margin-top: 100px">
     <div class="col-3">
         <?php require_once("../src/MyPage/mypage_wishlist.php"); ?>
     </div>
     <div class="col-8">
+        <div class="container catalog-breadcrumbs">
+                <a href="my_page.php"> My Page </a>
+        </div>
         <div class="row">
             <?php require_once("../src/MyPage/mypage_details.php"); ?>
         </div>

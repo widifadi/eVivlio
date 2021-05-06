f714be939cbe1e64c18e48f3830ecf99c3ad45bb
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-06 20:21:31 +0200

    Update contact.php

--- public/contact.php
+++ public/contact.php
@@ -3,20 +3,8 @@
 ?>
 
 
-<div class="row" style="margin-top: 100px">
-    <div class="col-3">
-        <?php require_once("../templates/contact_side_btn.php"); ?>
-    </div>
-    <div class="col-8">
-        <div class="container catalog-breadcrumbs">
-                <a href="contact.php"> Contact Us </a>
-        </div>
-        <div class="row">
-            <?php require_once("../src/ContactUs/contact_contacts.php"); ?>
-        </div>
-        <br>
-    </div> 
-</div>
+
+<?php require_once("../src/ContactUs/contact_us.php"); ?>
 
 <?php 
     require_once("../templates/footer.php");

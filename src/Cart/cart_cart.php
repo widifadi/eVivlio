a36d09f96f435e85bdc6c5730a583823e062493a
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-06 17:23:48 +0200

    Create cart_cart.php

---
+++ src/Cart/cart_cart.php
@@ -0,0 +1,72 @@
+<div class="container" style="margin-top:100px;">
+    <div class="row">
+        <div class="col-5" style="margin-left:-200px;">
+            <div class="row py-5 p-4">
+                <div class="col">
+                <div class="bg-light rounded-pill px-4 py-3 font-weight-bold">Order summary </div>
+                <div class="p-4">
+                    <ul class="list-unstyled mb-4">
+                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted" id="cart-number">Number of positions </strong><strong>5</strong></li>
+                    <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted" id="cart-total">Total</strong>
+                        <h5 class="font-weight-bold">$$</h5>
+                    </li>
+                    </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Procceed to checkout</a>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-9">
+            <div class="row">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                            <th scope="row" class="border-0">
+                                <div class="p-2">
+                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                                <div class="ml-3 d-inline-block align-middle">
+                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
+                                </div>
+                                </div>
+                            </th>
+                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
+                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
+                            </tr>
+                            <tr>
+                            <th scope="row" >
+                                <div class="p-2">
+                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                                <div class="ml-3 d-inline-block align-middle">
+                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
+                                </div>
+                                </div>
+                            </th>
+                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
+                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
+                            </tr>
+                            <tr>
+                            <th scope="row" >
+                                <div class="p-2">
+                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+                                <div class="ml-3 d-inline-block align-middle">
+                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
+                                </div>
+                                </div>
+                            </th>
+                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
+                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
+                            </tr>
+                    </tbody>
+                    </table>
+                </div> 
+            </div>
+        </div>
+    </div>
+</div>

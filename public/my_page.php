1cb0a2f9f4ea097babfb47323301e12ae22040c7
dashkovva <83634478+dashkovva@users.noreply.github.com>
2021-05-06 13:32:34 +0200

    Update my_page.php

--- public/my_page.php
+++ public/my_page.php
@@ -3,23 +3,8 @@
 ?>
 
 
-<div class="row" style="margin-top: 100px">
-    <div class="col-3">
-        <?php require_once("../src/MyPage/mypage_wishlist.php"); ?>
-    </div>
-    <div class="col-8">
-        <div class="container catalog-breadcrumbs">
-                <a href="my_page.php"> My Page </a>
-        </div>
-        <div class="row">
-            <?php require_once("../src/MyPage/mypage_details.php"); ?>
-        </div>
-        <br>
-        <div class="row">
-            <?php require_once("../src/MyPage/mypage_order_history.php"); ?>
-        </div>
-    </div> 
-</div>
+
+<?php require_once("../src/MyPage/mypage_details.php"); ?>
 
 <?php 
     require_once("../templates/footer.php");

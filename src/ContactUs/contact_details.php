528a1673ed650a1b357acb1dd8ab466165c52685
dashkovva <tretyakovadasha@gmail.com>
2021-05-14 21:35:09 +0200

    split contact us pages

---
+++ src/ContactUs/contact_details.php
@@ -0,0 +1,32 @@
+<div class="container catalog-breadcrumbs">
+    <a href="contact.php"> Contact Us </a> 
+</div>     
+<div class="container mt-3" id="contacts">
+    <div class="faq-text" style= "font-size: 16px"id="contact-text"> Looking for product support?  Get in touch with us! </div>
+    <br>
+    <div class="row mb-3">
+        <div class="col-1">
+        <i class="fas fa-phone-alt i-yellow"></i>
+        </div>
+
+        <div class="col-8 contact-details">
+        999 999 9999
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-1">
+        <i class="fas fa-envelope i-yellow"></i>
+        </div>
+        <div class="col-8 contact-details">
+            <a class="title-link" href="mailto:contact@eVivlio.com"><span>contact@eVivlio.com</span></a>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-1">
+        <i class="fas fa-map-marked i-yellow"></i>
+        </div>
+        <div class="col-8 contact-details">
+        14th km Thessaloniki, Nea Moudania 570 01, Greece
+        </div>
+    </div>
+</div>

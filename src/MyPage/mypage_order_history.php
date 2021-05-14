ae32f3c4e5216ea115d2367d4acb2f56ac7f4e98
dashkovva <tretyakovadasha@gmail.com>
2021-05-14 20:13:56 +0200

    split mypage content into separate pages

--- src/MyPage/mypage_order_history.php
+++ src/MyPage/mypage_order_history.php
@@ -1,41 +1,71 @@
-<div class="container mt-3" id="order-history">
-    <a class="title-link" href="order_history.php"> <span class="subtitle" style="font-size: 16px;">Order History</span></a>
-        <div class="row"> 
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-            <div class="col wl-book">
-                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                <br>
-                <div id="book-title">"Title", Author (Year)</div>
-            </div>
-  
+<div class="container catalog-breadcrumbs">
+    <a href="my_page.php"> My Page </a> 
+    <i class="fas fa-chevron-right" style="color: grey;"></i>
+    <a href=""> Order History </a> 
+
+</div> 
+<div class="col "> 
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
         </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <br>
+            <div id="book-title">"Title", Author (Year)</div>
+        </div>
+    </div>
 </div>
---
+++ src/MyPage/mypage_wishlist.php
@@ -0,0 +1,105 @@
+<div class="container catalog-breadcrumbs">
+    <a href="my_page.php"> My Page </a> 
+    <i class="fas fa-chevron-right" style="color: grey;"></i>
+    <a href=""> My Wishlist </a> 
+</div> 
+<div class="col "> 
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row wl-book">
+        <div class="col-2">
+            <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
+        </div>
+        <div class="col-4">
+            <div class="row mt-3 ml-3">
+                <div id="book-title"> <a href="#" class="text-dark">"Title", Author (Year)</a></div>
+            </div>
+            <div class="row mt-3 ml-3">
+                <a href="#" class="text-dark"><i class="fa fa-shopping-cart"></i></a>
+                <a href="#" class="text-dark ml-5"><i class="fa fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+</div>

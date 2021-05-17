567fe9054f0ea010d4bc53ebda9c889c2ae98820
EngrAAhmed <engraqeel2017@gmail.com>
2021-05-17 16:29:17 +0300

    .\database\database_functions.php

--- public/admin_page.php
+++ public/admin_page.php
@@ -2,8 +2,8 @@
     require_once("../templates/header.php"); 
 
     // restrict access
-    session_start();
-    if (!isset($_SESSION['user'])|| $_SESSION['admin_permission'] == 0) {
+ //   session_start();
+    if (!isset($_SESSION['user'])|| $_SESSION['user_permission'] == 0) {
         header("location: index.php");
     }
 ?>

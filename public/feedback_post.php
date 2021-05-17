08f6c3325320944928b38f79909151cdfa5e0bbf
dashkovva <tretyakovadasha@gmail.com>
2021-05-17 20:12:24 +0200

    fix feedback

--- public/feedback_post.php
+++ public/feedback_post.php
@@ -14,24 +14,21 @@
    
 
     if (isset($_POST['btn_feedback'])) {
-        $first_name = $_POST['feedback_fname'];
-        $first_name = mysqli_real_escape_string($conn, $first_name);
+        $feedback_name = $_POST['feedback_name'];
+        $feedback_name = mysqli_real_escape_string($conn, $feedback_name);
 
-        $last_name = $_POST['feedback_lname'];
-        $last_name = mysqli_real_escape_string($conn, $last_name);
-
-        $email = $_POST['feedback_email'];
-        $email = mysqli_real_escape_string($conn, $email);
+        $feedback_email = $_POST['feedback_email'];
+        $feedback_email = mysqli_real_escape_string($conn, $feedback_email);
 
 
-        $feedback = $_POST['feedback_text'];
-        $feedback = mysqli_real_escape_string($conn, $feedback);
+        $feedback_message = $_POST['feedback_message'];
+        $feedback_message = mysqli_real_escape_string($conn, $feedback_message);
 
 
-        $feedback_query = "INSERT INTO feedback (first_name, last_name, email, feedback) 
-                        VALUES ('$first_name', '$last_name', '$email', '$feedback')";
+        $feedback_query = "INSERT INTO feedback (feedback_name, feedback_email, feedback_message) 
+                        VALUES ('$feedback_name', '$feedback_email', '$feedback_message')";
         if ($conn->query($feedback_query) === TRUE) {
-            echo "New customer created successfully. <br>" . $first_name;
+            echo "New feedback from. <br>" . $feedback_name;
         } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
         }

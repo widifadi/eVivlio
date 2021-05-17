8d7380bb61473bfb934e830b0d813e66fea9367a
dashkovva <tretyakovadasha@gmail.com>
2021-05-17 12:14:38 +0200

    about us created

---
+++ src/AboutUs/about_team.php
@@ -0,0 +1,49 @@
+<div class="container catalog-breadcrumbs mb-3">
+        <a href="about.php"> About Us </a> 
+        <i class="fas fa-chevron-right" style="color: grey;"></i>
+        <a href=""> About the Team </a> 
+</div>
+<div class="card-columns" id="team">
+
+    <div class="card text-center p-1 book" id="Aqeel_Ahmed">
+        <img class="card-img-top book-cover" src="../assets/img/team/aqeel.png" 
+            alt="Aqeel Ahmed" ><br>
+        <span class="book-title">Aqeel Ahmed</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:aahmed@ihu.edu.gr">Contact me</a></span>
+    </div>
+    
+    <div class="card text-center p-1 book" id="Widiatmoko_Azis_Fadilah">
+        <img class="card-img-top book-cover" src="../assets/img/team/widi.png" 
+            alt="Widiatmoko Azis Fadilah" ><br>
+        <span class="book-title">Widiatmoko Azis Fadilah</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:wfadilah@ihu.edu.gr">Contact me</a></span>
+    </div>
+    
+    <div class="card text-center p-1 book" id="Hammad_Farooq">
+        <img class="card-img-top book-cover" src="../assets/img/team/hammad.jpg" 
+            alt="Hammad Farooq" ><br>
+        <span class="book-title">Hammad Farooq</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:hfarooq@ihu.edu.gr">Contact me</a></span>
+    </div>
+
+    <div class="card text-center p-1 book" id="Sheena_Fernandez">
+        <img class="card-img-top book-cover" src="../assets/img/team/shin.png" 
+            alt="Sheena Fernandez" ><br>
+        <span class="book-title">Sheena Fernandez</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:sfernandez@ihu.edu.gr">Contact me</a></span>
+    </div>
+
+    <div class="card text-center p-1 book" id="Komar_Javanmardi">
+        <img class="card-img-top book-cover" src="../assets/img/team/komar.png" 
+            alt="Komar Javanmardi" ><br>
+        <span class="book-title">Komar Javanmardi</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:kjavanmardi@ihu.edu.gr">Contact me</a></span>
+    </div>
+
+    <div class="card text-center p-1 book" id="Darya_Tretyakova">
+        <img class="card-img-top book-cover" src="../assets/img/team/dasha.jpg" 
+            alt="Darya Tretyakova" ><br>
+        <span class="book-title">Darya Tretyakova</span> <br>
+        <span class="badge badge-pill badge-secondary book-price"><a href="mailto:dtretyakova@ihu.edu.gr">Contact me</a></span>
+    </div>
+</div>

f0548703f178d00d049c58dc376694407259b81c
widifadi <widiatmokoazis@gmail.com>
2021-05-17 18:14:47 +0300

    preliminary cart functionality

--- src/Cart/cart_cart.php
+++ src/Cart/cart_cart.php
@@ -1,4 +1,10 @@
+<?php
+    session_start();
+?>
+
 <div class="container" style="margin-top:100px; margin-left: 10px;">
+
+    <!-- Order Summary Section -->
     <div class="row">
         <div class="col-4" style="">
             <div class="row">
@@ -16,6 +22,7 @@
             </div>
         </div>
 
+        <!-- My Cart Section -->
         <div class="col-8">
             <div class="container catalog-breadcrumbs">
                 <a href="my_page.php"> My Cart </a> 
@@ -24,6 +31,15 @@
                 <div class="table-responsive">
                     <table class="table" >
                         <tbody >
+                                <?php
+                                    // Fetching information from cart table 
+                                    require 'database_functions.php';
+                                    $stmt = $conn->prepare("SELECT * FROM cart");
+                                    $stmt->execute();
+                                    $result = $stmt->get_result();
+                                    $grand_total = 0;
+                                    while ($row = $result->fetch_assoc()):
+                                ?>
                             <tr >
                             <th scope="row" class="border-0" >
                                 <div class="p-2">
@@ -38,49 +54,8 @@
                             <td class="border-0 align-middle book-price" style="background:white;"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
                             <td class="border-0 align-middle book-price" style="background:white;"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
                             </tr>
-                            <tr>
-                            <tr>
-                            <th scope="row" class="border-0">
-                                <div class="p-2">
-                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                                <div class="ml-3 d-inline-block align-middle">
-                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
-                                </div>
-                                </div>
-                            </th>
-                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
-                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
-                            </tr>
-                            <tr>
-                            <th scope="row" >
-                                <div class="p-2">
-                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                                <div class="ml-3 d-inline-block align-middle">
-                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
-                                </div>
-                                </div>
-                            </th>
-                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
-                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
-                            </tr>
-                            <tr>
-                            <th scope="row" >
-                                <div class="p-2">
-                                <img src="../assets/img/open-book.png" alt="book" width="100px" id="book-cover">
-                                <div class="ml-3 d-inline-block align-middle">
-                                    <a href="#" class="text-dark"><div class="book-title" id="book-title">"Title", Author (Year)</div></a>
-                                </div>
-                                </div>
-                            </th>
-                            <td class="border-0 align-middle book-price" id="book-price"><strong>$$</strong></td>
-                            <td class="border-0 align-middle book-price" id="book-quantity"><strong>0</strong></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
-                            <td class="border-0 align-middle book-price"><a href="#" class="text-dark"><i class="fa fa-heart"></i></a></td>
-                            </tr>
+                                <?php endwhile; ?>
+                            
                         </tbody>
                     </table>
                 </div> 
@@ -88,3 +63,43 @@
         </div>
     </div>
 </div>
+
+<!-- Ajax Code for cart -->
+<script type="text/javascript">
+        $(document).ready(function(){
+
+            $(".itemQty").on('change',function(){
+                var $el = $(this).closest('tr');
+
+                var pid = $el.find(".pid").val();
+                var pprice = $el.find(".pprice").val();
+                var qty = $el.find(".itemQty").val();
+                console.log(qty);
+                location.reload(true);
+
+                $.ajax({
+                    url: 'action.php',
+                    method: 'post',
+                    cache: false,
+                    data: {qty:qty, pid:pid, pprice:pprice},
+                    success: function(response){
+                        console.log(response);
+                    }
+                })
+            });
+
+            load_cart_item_number();
+
+            // function to display item number on cart icon 
+            function load_cart_item_number(){
+                $.ajax({
+                    url: 'action.php',
+                    method: 'get',
+                    data: {cartItem:"cart_item"},
+                    success: function(response){
+                        $("#cart-item").html(response);
+                    }
+                });
+            }
+        });
+    </script>

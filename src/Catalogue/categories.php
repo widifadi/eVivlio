9a1ea3e9d73511f410ecf7ce0583ee8f5101a96a
Hammad Farooq <hammad.farooq1994@gmail.com>
2021-05-18 04:04:33 +0300

    On branch develop

--- src/Catalogue/categories.php
+++ src/Catalogue/categories.php
@@ -5,20 +5,20 @@
   <ul class="list-group list-group-flush">
   
     <li class="list-group-item category-name" id="business-category" >
-    <a  href="?p=Business">Business</a> </li>
+    <a  href="../public/catalog.php?p=Business">Business</a> </li>
     <li class="list-group-item category-name" id="childrens-category">
-    <a href="?p=Children Collection">Children's Collection</a> </li>
+    <a href="../public/catalog.php?p=Children Collection">Children's Collection</a> </li>
     <li class="list-group-item category-name" id="history-category">
-    <a href="?p=History">History</a> </li>
+    <a href="../public/catalog.php?p=History">History</a> </li>
     <li class="list-group-item category-name" id="literature-category">
-    <a href="?p=Literature">Literature</a> </li>
+    <a href="../public/catalog.php?p=Literature">Literature</a> </li>
     <li class="list-group-item category-name" id="novels-category">
-    <a href="?p=Novels">Novels</a> </li>
+    <a href="../public/catalog.php?p=Novels">Novels</a> </li>
     <li class="list-group-item category-name" id="scifi-category">
-    <a href="?p=Science Fiction">Science Fiction</a> </li>
+    <a href="../public/catalog.php?p=Science Fiction">Science Fiction</a> </li>
     <li class="list-group-item category-name" id="scitech-category">
-    <a href="?p=Science and Technology">Science & Technology</a> </li>
+    <a href="../public/catalog.php?p=Science and Technology">Science & Technology</a> </li>
     <li class="list-group-item category-name" id="philosophy-category">
-    <a href="?p=Philosophy">Philosophy</a> </li>
+    <a href="../public/catalog.php?p=Philosophy">Philosophy</a> </li>
 </ul>
 </div>

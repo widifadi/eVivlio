2c85fff7139b2442eadfc08f1fdf8e1722b7b4a9
dashkovva <tretyakovadasha@gmail.com>
2021-05-24 23:08:16 +0200

    about us updated

--- src/AboutUs/about_store.php
+++ src/AboutUs/about_store.php
@@ -4,6 +4,9 @@
         <a href=""> About the Store </a> 
 </div>
 <div class="container">
+    <div class="text mb-2 about-text"> &nbsp &nbsp The name <i>eVivlio</i> comes from a greek word  
+    "Βιβλίο" which means "library". And "e" makes it into a new online format.
+    </div>
     <div class="text mb-2 about-text"> &nbsp &nbsp eVivlio is an online bookstore brought to you 
         by six awesome students under Smart Cities and Communities  Proggramme. 
     </div>
@@ -11,6 +14,8 @@
         &nbsp &nbsp We believe that Smart Cities start with you!
     </div>
     <div class="text mb-2 about-text">
-        &nbsp &nbsp So you can make the world a smatter place by enjoying your favourite books!
+        &nbsp &nbsp So you are the person whi can make the world a smatter 
+        place by simply enjoying your favourite books!
      </div>
+     <img class="w-100" src="../assets/img/team/about.jpg" alt="Coming soon">
 </div>  

ece569de6665a0b4bbb097fa54e7006eeb7ef11b
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-05-31 01:57:20 +0300

    Fix for #36

--- src/Common/book_card.php
+++ src/Common/book_card.php
@@ -15,23 +15,29 @@
         $author = $author_row['author_first_name'] . $author_row['author_last_name'];
 ?>
 
-        <a href="book_details.php?bookid=<?php echo $book_id?>">
-            <div class="card text-center p-1 book">
-                <img class="card-img-top book-cover" src='<?php echo $book_cover ?>'
-                    alt='<?php echo $book_item['book_title'] ?>' >
-                <br>
-                <span class="book-title"><?php echo $book_item['book_title'] ?></span> <br>
-                <span class="book-author">
-                    <?php echo $author ?>
-                    (<?php echo $book_item['publishing_year'] ?>)</span>
-                <br>
-                <span class="badge badge-pill badge-secondary book-price">€<?php echo $book_item['price'] ?></span>
-                <br><br>
-                <em class="fas fa-cart-plus add-cart-btn"></em>
-                <em class="fas fa-heart add-wlist-btn"></em>
+<a href="book_details.php?bookid=<?php echo $book_id?>">
+    <div class="card text-center p-1 book">
+        <img class="card-img-top book-cover" src='<?php echo $book_cover ?>'
+            alt='<?php echo $book_item['book_title'] ?>' >
+        <br>
+        <span class="book-title"><?php echo $book_item['book_title'] ?></span> <br>
+        <span class="book-author">
+            <?php echo $author ?>
+            (<?php echo $book_item['publishing_year'] ?>)</span>
+        <br>
+        <span class="badge badge-pill badge-secondary book-price">€<?php echo $book_item['price'] ?></span>
+        <br><br>
+        <em class="fas fa-cart-plus add-cart-btn"></em>
 
-            </div>
-        </a>
+        <?php 
+            if (isset($_SESSION['user'])) {
+        ?>
+            <em class="fas fa-heart add-wishlist-btn"></em>
+        <?php
+            }
+        ?>
+    </div>
+</a>
         
 <?php
     }

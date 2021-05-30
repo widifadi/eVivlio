a044e77196cfe1f144617d2a7194d92a6876afb8
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-05-31 01:35:45 +0300

    Make book_item_card function for book cards element

--- src/Catalogue/all_books.php
+++ src/Catalogue/all_books.php
@@ -1,33 +1,25 @@
 
 <div class="card-columns" id="catalog">
- <?php
-    $conn = db_connection();
-    $query= 'SELECT book_id, book_cover, book_title, price FROM book';
-    $query_run= mysqli_query($conn,$query);
-    $check_books=mysqli_num_rows($query_run) > 0;
-    if($check_books){
-    while($row=mysqli_fetch_assoc($query_run))
-    {
- ?>
-    <a href="book_details.php?bookid=<?php echo $row['book_id']?>">
-        <div class="card text-center p-1 book" >
-          <img class="card-img-top book-cover" src="../assets/img/book-covers/<?php echo $row['book_cover'];?>" 
-                alt="<?php echo $row['book_cover'];?>" >
-                <br>
-                <span class="book-title"><?php echo $row['book_title'];?></span> <br>
-                <!--<span class="book-author">J. R. R. Tolkien (1995)</span>
-                <br>-->
-                <span class="badge badge-pill badge-secondary book-price"><?php echo $row['price'];?></span>
-        </div>
-    </a>
- <?php
-     }}
-     
 
-    else{
-    echo "no book found";
-    }
-    $conn->close();
- ?>
+<?php
+   require_once("../src/Common/book_card.php");
+
+   $conn = db_connection();
+   $query= 'SELECT book_id FROM book';
+   $query_run= mysqli_query($conn,$query);
+   $check_books=mysqli_num_rows($query_run) > 0;
+
+   if($check_books) {
+      while ($row=mysqli_fetch_assoc($query_run)) {
+         $book_id = $row['book_id'];
+         book_item_card($conn, $book_id);
+      }
+   } else {
+      echo "No book found.";
+   }
+   
+   $conn->close();
+?>
+
 </div>
   
--- src/Catalogue/specific_books.php
+++ src/Catalogue/specific_books.php
@@ -1,34 +1,26 @@
 
 <div class="card-columns" id="catalog">
+
 <?php
+    require_once("../src/Common/book_card.php");
     
-   $conn = db_connection();
-   $query= "SELECT book.book_id, book.book_cover, book.price, book.book_title FROM book JOIN category_tag 
-   ON category_tag.book_id = book.book_id JOIN category ON category_tag.category_id = category.category_id 
-   WHERE category.category_name='$category'";
+    $conn = db_connection();
+    $query= "SELECT book.book_id FROM book 
+               JOIN category_tag ON category_tag.book_id = book.book_id 
+               JOIN category ON category_tag.category_id = category.category_id 
+               WHERE category.category_name='$category'";
    $query_run= mysqli_query($conn,$query);
    $check_books=mysqli_num_rows($query_run) > 0;
-   if($check_books){
-   while($row=mysqli_fetch_assoc($query_run))
-   {
-?>
-   <a href="book_details.php?bookid=<?php echo $row['book_id']?>">
-      <div class="card text-center p-1 book" >
-         <img class="card-img-top book-cover" src="../assets/img/book-covers/<?php echo $row['book_cover'];?>" 
-               alt="<?php echo $row['book_cover'];?>" >
-               <br>
-               <span class="book-title"><?php echo $row['book_title'];?></span> <br>
-               <!--<span class="book-author">J. R. R. Tolkien (1995)</span>
-               <br>-->
-               <span class="badge badge-pill badge-secondary book-price"><?php echo $row['price'];?></span>
-      </div>
-   </a>
- <?php
+   if ($check_books) {
+      while ($row=mysqli_fetch_assoc($query_run)) {
+         $book_id = $row['book_id'];
+         book_item_card($conn, $book_id);
      }
    } else {
       echo "No book found.";
    }
       $conn->close();
- ?>
+?>
+
 </div>
   

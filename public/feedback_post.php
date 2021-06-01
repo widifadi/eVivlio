33eab436f64956894bb24e11052712c9d1715aea
dashkovva <tretyakovadasha@gmail.com>
2021-06-01 18:09:38 +0200

    #41

--- public/feedback_post.php
+++ public/feedback_post.php
@@ -1,15 +1,7 @@
 <?php require_once("../templates/header.php"); ?>
 <?php 
-    // TODO do sql connection only once for the whole app
-    // TODO use database_functions file
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "evivlio";
-
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    require_once("../database/database_functions.php");
+    $conn = db_connection();
 
    
 

c3bd11a837b61e5f2ad323fbc231e476ad77a381
dashkovva <tretyakovadasha@gmail.com>
2021-06-01 20:08:50 +0200

    Update feedback_post.php

--- public/feedback_post.php
+++ public/feedback_post.php
@@ -1,11 +1,4 @@
 <?php require_once("../templates/header.php"); ?>
-<?php
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-?>
-<html>
-<body>
 
 
 <?php 

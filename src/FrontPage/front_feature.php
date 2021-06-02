c38d5ed5f938c3e387356369f83a0a94390d859e
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-06-02 08:33:16 +0300

    Fix frontpage features classes #37

--- src/FrontPage/front_feature.php
+++ src/FrontPage/front_feature.php
@@ -1,36 +1,33 @@
 
 <?php require_once("../database/database_functions.php"); ?>
-<?php require_once("../src/Common/book_card.php"); ?>
 
-<div class="container features-div">
+<div class=" features-div">
 
-    <div id="best-sellers">
+    <div id="best-sellers" class="frontpage-feature">
         <span class="subtitle">Best Sellers</span>
-          <div class="row">
-              <?php
-                  $conn = db_connection();
-                  $best_sellers = get_featured_books_list($conn, "Best Sellers");
+            <div class="row">
+                <?php
+                    $conn = db_connection();
+                    $best_sellers = get_featured_books_list($conn, "Best Sellers");
 
-                  $counter = 0;
-                  foreach($best_sellers as $key=>$best_seller_id) {
-                    if (++$counter == 6) {
-                        break;
-                    }
-              ?>
-                  <div class="col-sm featured-book-div">
-                      <?php
-                          $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
-                          $book_result = mysqli_query($conn, $book_query);
-                          $book_item = mysqli_fetch_assoc($book_result);
-                          $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
-                      ?>
-                      <div class="card text-center p-1 book">
-                      <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
-                          <img class="card-img-top book-cover" src='<?php echo $book_cover ?>'
-                              alt='<?php echo $book_item['book_title'] ?>' width="100" >
-                          <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
-                      </a>
-                      </div>
+                    $counter = 0;
+                    foreach($best_sellers as $key=>$best_seller_id) {
+                        if (++$counter == 6) {
+                            break;
+                        }
+                ?>
+                    <div class="col-sm featured-book-div">
+                        <?php
+                            $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
+                            $book_result = mysqli_query($conn, $book_query);
+                            $book_item = mysqli_fetch_assoc($book_result);
+                            $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
+                        ?>
+                        <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
+                            <img class="feature-book-cover featured-book-image" src='<?php echo $book_cover ?>'
+                                alt='<?php echo $book_item['book_title'] ?>' >
+                            <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
+                        </a>
                     </div>    
               <?php 
                   }
@@ -38,7 +35,7 @@
           </div>
     </div>
 
-    <div id="new-release">
+    <div id="new-release" class="frontpage-feature">
         <span class="subtitle">New Release</span>
         <div class="row">
               <?php
@@ -50,58 +47,55 @@
                     if (++$counter == 6) {
                         break;
                     }
-              ?>
-                  <div class="col-sm featured-book-div">
-                      <?php
-                          $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
-                          $book_result = mysqli_query($conn, $book_query);
-                          $book_item = mysqli_fetch_assoc($book_result);
-                          $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
-                      ?>
-                      <div class="card text-center p-1 book">
-                      <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
-                          <img class="card-img-top book-cover" src='<?php echo $book_cover ?>'
-                              alt='<?php echo $book_item['book_title'] ?>' width="100" >
-                          <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
-                      </a>
-                      </div>
+                ?>
+                    <div class="col-sm featured-book-div">
+                        <?php
+                            $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
+                            $book_result = mysqli_query($conn, $book_query);
+                            $book_item = mysqli_fetch_assoc($book_result);
+                            $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
+                        ?>
+                        <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
+                            <img class="feature-book-cover featured-book-image" src='<?php echo $book_cover ?>'
+                                alt='<?php echo $book_item['book_title'] ?>' >
+                            <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
+                        </a>
                     </div>    
-              <?php 
-                  }
-              ?>
+                <?php 
+                    }
+                ?>
           </div>
     </div>
-    <div id="editors-pick" >
+
+    <div id="editors-pick" class="frontpage-feature">
         <span class="subtitle">Editor's Pick</span>
         <div class="row">
-              <?php
-                  $conn = db_connection();
-                  $best_sellers = get_featured_books_list($conn, "New Release");
+            <?php
+                $conn = db_connection();
+                $best_sellers = get_featured_books_list($conn, "New Release");
 
-                  $counter = 0;
-                  foreach($best_sellers as $key=>$best_seller_id) {
-                    if (++$counter == 6) {
-                        break;
-                    }
-              ?>
-                  <div class="col-sm featured-book-div">
-                      <?php
-                          $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
-                          $book_result = mysqli_query($conn, $book_query);
-                          $book_item = mysqli_fetch_assoc($book_result);
-                          $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
-                      ?>
-                      <div class="card text-center p-1 book">
-                      <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
-                          <img class="card-img-top book-cover" src='<?php echo $book_cover ?>'
-                              alt='<?php echo $book_item['book_title'] ?>' width="100" >
-                          <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
-                      </a>
-                      </div>
-                    </div>    
-              <?php 
-                  }
-              ?>
+                $counter = 0;
+                foreach($best_sellers as $key=>$best_seller_id) {
+                if (++$counter == 6) {
+                    break;
+                }
+            ?>
+                <div class="col-sm featured-book-div">
+                    <?php
+                        $book_query = "SELECT * FROM book WHERE book_id=$best_seller_id;";
+                        $book_result = mysqli_query($conn, $book_query);
+                        $book_item = mysqli_fetch_assoc($book_result);
+                        $book_cover = '../assets/img/book-covers/' . $book_item['book_cover'];
+                    ?>
+                    <a href="book_details.php?bookid=<?php echo $best_seller_id?>">
+                        <img class="feature-book-cover featured-book-image" src='<?php echo $book_cover ?>'
+                            alt='<?php echo $book_item['book_title'] ?>' >
+                        <div class="featured-book-middle"><?php echo $book_item['book_title'] ?></div>
+                    </a>
+                    </div>
+            <?php 
+                }
+            ?>
           </div>
     </div>
 

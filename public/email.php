d16dd8dfdfc56e2451507880619e81b344964071
Hammad Farooq <hammad.farooq1994@gmail.com>
2021-06-04 03:28:39 +0300

    On branch develop

--- public/email.php
+++ public/email.php
@@ -10,36 +10,37 @@ $txt ="
 <p>Receipt</p>
 
 <table>
-<tr style="."padding-bottom: 5em".">
+<tr style=".'"padding-bottom: 5em"'.">
 <th>Name</th>
-<th style="."text-align: center;padding-right: 4em;"."> Order Number </th>
-<th style="."text-align: center; padding-right: 4em;".">Date Date </th>
+<th style=".'"text-align: center;padding-right: 4em;"'."> Order Number </th>
+<th style=".'"text-align: center; padding-right: 4em;"'.">Date Date </th>
 </tr>
-    <tr style="."padding-bottom: 3em".">
+    <tr style=".'"padding-bottom: 6em"'.">
     <td >".$CustomerName. "</td>
-    <td style="."text-align: center".">".$orderID. "</td>
-    <td style="."text-align: center".">".$Date."</td>
+    <td style=".'"text-align: center"'.">".$orderID. "</td>
+    <td style=".'"text-align: center"'.">".$Date."</td>
     </tr>
-<tr style="."padding-bottom: 2em".">
+<tr style=".'"padding-bottom: 6em"'.">
 <th>Book Title</th>
-<th style="."text-align: center".">Quantity</th>
-<th style="."text-align: center"."> Price</th>".
- /*</tr>
-
-    <?php for($p=0; $p<count($qtys); $p++){?>
-<tr style="."padding-bottom: 2em".">
-    <td><?php echo $bookTitles[$p];?></td>
-    <td style="."text-align: center"."><?php echo $qtys[$p];?></td>
-    <td style="."text-align: center"."><?php echo$prices[$p];?></td>
+<th style=".'"text-align: center"'.">Quantity</th>
+<th style=".'"text-align: center"'."> Price</th>
+ </tr>";
+     $totalSum=0;
+    for($p=0; $p<count($qtys); $p++){
+   $txt.="<tr style=".'"padding-bottom: 2em"'.">
+    <td>".$bookTitles[$p]."</td>
+    <td style=".'"text-align: center"'.">".$qtys[$p]."</td>
+    <td style=".'"text-align: center"'.">".$prices[$p]."</td>
     
-</tr>
-    <?php
+</tr>";
     
-$totalSUM+=$prices[$p];
+    
+    $totalSum+=$prices[$p];
 
-}?>
-<tr style="."padding-bottom: 10em".">
-<td><th>Total Price</th> </td><td style="."text-align: center"."><?php echo $totalSUM?> </tr>*/
+}
+
+ $txt.= "<tr style=".'"padding-bottom: 10em"'.">
+<td><th>Total Price</th> </td><td style=".'"text-align: center"'.">".$totalSum ."</tr>".
 
 "</table>
 

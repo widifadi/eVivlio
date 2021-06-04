6280db388f0aa3ffe53253cda0f3142579832138
widifadi <widiatmokoazis@gmail.com>
2021-06-04 15:53:43 +0300

    moving back alert from header

--- assets/js/bootstrapAlert.js
+++ assets/js/bootstrapAlert.js
@@ -1,4 +1,5 @@
-src = "https://cdnjs.cloudflare.com/ajax/libs/bootsrap-growl/1.0.0/jquery.bootsrap-growl.min.js"
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js" integrity="sha512-pBoUgBw+mK85IYWlMTSeBQ0Djx3u23anXFNQfBiIm2D8MbVT9lr+IxUccP8AMMQ6LCvgnlhUCK3ZCThaBCr8Ng==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 function bootstrapAlert() {
     $.bootstrapGrowl("This is success message!",{
         type: "success",

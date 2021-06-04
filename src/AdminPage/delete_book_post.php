37fa83ab7a30fb038cade0808916cc3369a50e16
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-06-04 08:37:47 +0300

    Add delete queries for relations to delete functions

--- src/AdminPage/delete_book_post.php
+++ src/AdminPage/delete_book_post.php
@@ -8,12 +8,20 @@
     $delete_author_tag = "DELETE FROM author_tag WHERE book_id=$book_id;";
     $delete_category_tag = "DELETE FROM category_tag WHERE book_id=$book_id;";
     $delete_feature_tag = "DELETE FROM feature_tag WHERE book_id=$book_id;";
+    $delete_wishlist = "DELETE FROM wishlist WHERE book_id=$book_id;";
+    $delete_order_items = "DELETE FROM order_items WHERE book_id=$book_id;";
+    $delete_book_review = "DELETE FROM book_review WHERE book_id=$book_id;";
+    $delete_cart = "DELETE FROM cart WHERE book_id=$book_id;";
 
     $delete_book = "DELETE FROM book WHERE book_id=$book_id;";
 
     mysqli_query($conn, $delete_author_tag);
     mysqli_query($conn, $delete_category_tag);
     mysqli_query($conn, $delete_feature_tag);
+    mysqli_query($conn, $delete_wishlist);
+    mysqli_query($conn, $delete_order_items);
+    mysqli_query($conn, $delete_book_review);
+    mysqli_query($conn, $delete_cart);
 
     if (mysqli_query($conn, $delete_book)) {
         echo "0";
--- src/AdminPage/delete_customer_post.php
+++ src/AdminPage/delete_customer_post.php
@@ -5,6 +5,18 @@
     // Get POST data from AJAX
     $customer_id = $_POST['customer_id'];
 
+    $delete_cart = "DELETE FROM cart WHERE customer_id=$customer_id;";
+    $delete_book_review = "DELETE FROM book_review WHERE customer_id=$customer_id;";
+    $delete_customer_order = "DELETE FROM customer_order WHERE customer_id=$customer_id;";
+    $delete_payment = "DELETE FROM payment WHERE customer_id=$customer_id;";
+    $delete_wishlist = "DELETE FROM wishlist WHERE customer_id=$customer_id;";
+
+    mysqli_query($conn, $delete_cart);
+    mysqli_query($conn, $delete_book_review);
+    mysqli_query($conn, $delete_customer_order);
+    mysqli_query($conn, $delete_payment);
+    mysqli_query($conn, $delete_wishlist);
+
     $delete_customer = "DELETE FROM customer WHERE customer_id=$customer_id; "; 
     if (mysqli_query($conn, $delete_customer)) {
         echo "0";

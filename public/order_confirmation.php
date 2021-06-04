5e2feee3d0418ea6e00963fb5978dac835a7ab2b
Sheena Fernandez <sheena.fernandez78@gmail.com>
2021-06-04 11:22:26 +0300

    Fix order pages layout

--- public/order_confirmation.php
+++ public/order_confirmation.php
@@ -1,126 +1,135 @@
 <?php
-require_once("../templates/header.php"); ?>
+	require_once("../templates/header.php"); ?>
+
 <?php
- include("../database/database_functions.php");
- $conn = db_connection();
-$user_name = $_SESSION['user'];
-   
-        ?>
-      	
-	<?php
+	include("../database/database_functions.php");
+	$conn = db_connection();
+	$user_name = $_SESSION['user'];
+
 	$customer_query = "SELECT customer.email, customer.first_name, customer.last_name, customer.customer_id
 					    FROM user
-						JOIN
-						customer ON customer.customer_id=user.customer_id 
-						WHERE username= '$user_name'";
+						JOIN customer ON customer.customer_id=user.customer_id 
+						WHERE username= '$user_name'; ";
 
-			$result= mysqli_query($conn, $customer_query);
-			$result = mysqli_fetch_assoc($result);
+	$result= mysqli_query($conn, $customer_query);
+	$result = mysqli_fetch_assoc($result);
 
-			$customerID= $result['customer_id']; 
-			$CustomerName= $result['first_name']. " ". $result['last_name'];
-			$customerEmail= $result['email']; 
+	$customerID= $result['customer_id']; 
+	$CustomerName= $result['first_name'] . " " . $result['last_name'];
+	$customerEmail= $result['email']; 
 
-						
-	 $order_query= "SELECT	customer_order.order_id, customer_order.order_date
-					 FROM customer_order 
-					WHERE customer_id= '$customerID' ORDER BY  order_id DESC LIMIT 1
-					";
+	$order_query= "SELECT customer_order.order_id, customer_order.order_date
+				FROM customer_order 
+				WHERE customer_id= '$customerID' ORDER BY  order_id DESC LIMIT 1;";
 
-			$OrdeResult= mysqli_query($conn, $order_query);
-			$OrdeResult = mysqli_fetch_assoc($OrdeResult );
+	$OrdeResult= mysqli_query($conn, $order_query);
+	$OrdeResult = mysqli_fetch_assoc($OrdeResult );
 
-			$orderID= $OrdeResult['order_id']; 
-			$Date= $OrdeResult['order_date'];
+	$orderID= $OrdeResult['order_id']; 
+	$Date= $OrdeResult['order_date'];
 
-		$orderBook= "SELECT *
-					 FROM order_items 
-					 WHERE order_id= '$orderID'";
+	$orderBook= "SELECT * FROM order_items 
+				WHERE order_id= '$orderID'";
 
-		$orderBook= mysqli_query($conn, $orderBook);
-				
-				$qtys[]= array();
-				$prices[]= array();
-				$bookTitles[]= array();
-				$totalSUM=0;
-				$index=0;
-
-			 while($orderBooks = mysqli_fetch_assoc($orderBook))
-			 {
-					$qtys[$index] = $orderBooks['quantity']; 
-					$prices[$index]		 = $orderBooks['total_price'];					
-					$orderedBookID= $orderBooks['book_id'];
-
-					$BookQuery= "SELECT book_title
-					 FROM book 
-					 WHERE book_id= '$orderedBookID'";
-
-					$orderBookitem= mysqli_query($conn, $BookQuery);
-					$orderBookitem= mysqli_fetch_assoc(	$orderBookitem);
-
-					$bookTitles[$index]  = $orderBookitem['book_title'];	
-					$index++;				 
-			 }
-
-
-			 require_once("../public/email.php"); 
+	$orderBook= mysqli_query($conn, $orderBook);
 			
+	$qtys = array();
+	$prices = array();
+	$bookTitles = array();
+	$total_sum = 0;
+	$index = 0;
+
+	while($orderBooks = mysqli_fetch_assoc($orderBook)) {
+		$qtys[$index] 	= $orderBooks['quantity']; 
+		$prices[$index] = $orderBooks['total_price'];					
+		$orderedBookID	= $orderBooks['book_id'];
+
+		$BookQuery= "SELECT book_title FROM book 
+					WHERE book_id= '$orderedBookID'";
+
+		$orderBookitem= mysqli_query($conn, $BookQuery);
+		$orderBookitem= mysqli_fetch_assoc(	$orderBookitem);
+
+		$bookTitles[$index]  = $orderBookitem['book_title'];	
+		$index++;				 
+	}
+
+	// require_once("../public/email.php"); 
 ?>
 
-				<div class="container " style="margin-top: 100px; margin-bottom: 200px; width: 50%;"> 
-				<form  id="login-form">
 
-						<div > 	<p style="text-align: center"> <b> Thank You for Shopping with Us! </b> </p>
-								<p style="text-align: center"> <b> ORDER RECIEPT </b> </p>
+<div class="container order-confirmation-div" style="margin-top: 100px; margin-bottom: 200px; width: 50%;"> 
+	<p style="text-align:center; color: white;">Thank you for shopping with us!</p>
+	<p style="text-align:center; "><strong>ORDER RECEIPT</strong></p>
 
+	<div class="row ml-3" style="color:white">
+		<div class="col">Customer Details</div>
+	</div>
+	<div class="row ml-5" style="color:white">
+		<div class="col">
+			<strong>Name</strong><br>
+			<strong>Order Number</strong> <br>
+			<strong>Date</strong> <br>
 
-						  <table>
-								<tr style="padding-bottom: 5em">
-							<th><th  style="text-align: center;padding-left: 2em;">	<th>Name</th> </th> </th>
- 								<th style="text-align: center;padding-left: 2em;"> Order Number </th>
-								<th style="text-align: center; padding-right: 4em;"> Date </th>
-								</tr>
-										<tr style="padding-bottom: 3em">
-										<th><th><td ><?php echo  $CustomerName; ?></td></th></th>
-									<td style="text-align: center"><?php echo $orderID;?></td>
-									<td style="text-align: center"><?php echo $Date; ?></td>
-									</tr>
-								<tr style="padding-bottom: 2em">
-							<th><th><th>Book Title</th> </th>
-							
-								<th style="text-align: center">Quantity</th>
-								<th style="text-align: center"> Price</th>
-								</tr>
-									<?php for($p=0; $p<count($qtys); $p++){?>
-								<tr style="padding-bottom: 2em">
-								<td><td> <td><?php echo $bookTitles[$p];?></td> </td> </td>
-								<td style="text-align: center"><?php echo $qtys[$p];?> </td>
-									<td style="text-align: center;"><?php echo$prices[$p];?></td>									
-								</tr>
-									<?php
-								$totalSUM+=$prices[$p];
-								}?>
-							<tr>
-							<tr style="padding-right: 5em">
-							<th><th>	<th></th> </th> </th>
-								<th style="text-align: center;padding-left	: 15em;"> Total Price </th>
-								<td style="text-align: center ">	 <?php echo $totalSUM?> </td> 
-							</tr>
-						
-						</table>	
+		</div>
+		<div class="col">
+			<?php echo $CustomerName; ?>
+			<?php echo $orderID; ?>
+			<?php echo $Date; ?>
 
-								<div style="text-align:center"	>
-									<button onClick="window.print()">Print Reciept</button>
-				       			</div>		
-					   
-					  &nbsp;&nbsp;
-						<p style="text-align: center"> Please Check your email as well for order details </p>
+		</div>
+		<div class="col text-center">
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="row ml-3" style="color:white">
+		<div class="col">Book Details</div>
+	</div>
+	<div class="row text-center" style="color:white">
+		<div class="col">
+			<strong>Book Title</strong>
+		</div>
+		<div class="col">
+			<strong>Quantity</strong>
+		</div>
+		<div class="col">
+			<strong>Price</strong>
+		</div>
+	</div>
+	<?php 
+		for ($p=0; $p<count($qtys); $p++) {
+			echo $bookTitles[0];
+	?>
+		<div class="row text-center">
+			<div class="col"><?php echo $bookTitles[$p];?></div>
+			<div class="col"><?php echo $qtys[$p];?></div>
+			<div class="col"><?php echo $prices[$p];?></div>
+		</div>
+	<?php
+			$total_sum += $prices[$p];
+		}
+	?>
 
-					</div>
-				</form>
-				</div>
-				
+	<br>
+	<br>
+	<div class="row text-center">
+		<div class="col">
+			<strong>Total Price: </strong>
+			<?php echo $total_sum ?>
+		</div>
+	</div>
 
+	<br>
+	<br>
+	<div style="text-align:center"	>
+		<button class="btn btn-info" onClick="window.print()">Print Receipt</button>
+	</div>
 
+	&nbsp;&nbsp;
+	<p style="text-align: center">
+		Please check your email as well for order details.
+	</p>
+</div>
 
-	<?php require_once("../templates/footer.php"); 
+<?php require_once("../templates/footer.php"); ?>
--- public/payment_process.php
+++ public/payment_process.php
@@ -1,62 +1,57 @@
 <?php
-require_once("../templates/header.php");
+    require_once("../templates/header.php");
 ?>
+
 <?php
- include("../database/database_functions.php");
- $conn = db_connection();
- if (isset($_POST['save-order'])){
+    include("../database/database_functions.php");
+    $conn = db_connection();
+    if (isset($_POST['save-order'])) {
 
-    $user_name = $_SESSION['user'];
+        $user_name = $_SESSION['user'];
 
-    $payment_method=$_POST['card_type'];
-    $payment_method = mysqli_real_escape_string($conn, $payment_method);
+        $payment_method=$_POST['card_type'];
+        $payment_method = mysqli_real_escape_string($conn, $payment_method);
 
-    $card_number=$_POST['card_number'];
-    $card_number = mysqli_real_escape_string($conn, $card_number);
+        $card_number=$_POST['card_number'];
+        $card_number = mysqli_real_escape_string($conn, $card_number);
 
+        $PID= $_POST['card_PID'];
+        $PID = mysqli_real_escape_string($conn, $PID);
 
-    $PID= $_POST['card_PID'];
-    $PID = mysqli_real_escape_string($conn, $PID);
+        $order_date= $_POST['date'];
+        $order_date = mysqli_real_escape_string($conn, $order_date);
 
+        $customer_name=$_POST['card_owner'];
+        $customer_name=mysqli_real_escape_string($conn, $order_date);
 
-    $order_date= $_POST['date'];
-    $order_date = mysqli_real_escape_string($conn, $order_date);
-
-
-    $customer_name=$_POST['card_owner'];
-    $customer_name=mysqli_real_escape_string($conn, $order_date);
-
-   // $user_name=$_SESSION['user'];
-     $bookId[]=array();
-     $qty[]=array();
-     $totalPrice[]=array();
-     $custID[]=array();
-     $OrderID[]=array();
-
+        // $user_name=$_SESSION['user'];
+        $bookId[]=array();
+        $qty[]=array();
+        $totalPrice[]=array();
+        $custID[]=array();
+        $OrderID[]=array();
    
-/*
+        /*
         for( $i=0; $i<count($bookId); $i++){
             $custOrder_query= "INSERT INTO customer_order (customer_id, order_date, shipping_status) 
                             VALUES( '$custID', '$order_date', 'PLACED')";
                      mysqli_query($conn, $custOrder_query);
         }
+        */
 
+        $customer_query = "SELECT * FROM customer 
+                            INNER JOIN user 
+                            ON customer.customer_id=user.customer_id
+                            WHERE username='$user_name'"; 
+        $customer_result = mysqli_query($conn, $customer_query);
+        $customer_details = mysqli_fetch_assoc($customer_result);
+        $customer_ID = $customer_details['customer_id'];
 
-*/
-
-    $customer_query = "SELECT * FROM customer 
-            INNER JOIN user 
-            ON customer.customer_id=user.customer_id
-             WHERE username='$user_name'"; 
-            $customer_result = mysqli_query($conn, $customer_query);
-            $customer_details = mysqli_fetch_assoc($customer_result);
-            $customer_ID = $customer_details['customer_id'];
-
-    $payment_query = "INSERT INTO payment (customer_id, payment_date, payment_method)
-     VALUES  ('$customer_ID', '$order_date', '$payment_method')";
-                        
+        $payment_query = "INSERT INTO payment (customer_id, payment_date, payment_method)
+                            VALUES  ('$customer_ID', '$order_date', '$payment_method')";
+                            
         if ($conn->query($payment_query) === TRUE) {
-           header('location:order_confirmation.php');
+            header('location:order_confirmation.php');
         } else {
             echo "Customer Table Error: " . $sql . "<br>" . $conn->error . "<br>";
         }
@@ -64,35 +59,28 @@ require_once("../templates/header.php");
         $order_query = "SELECT * FROM cart WHERE customer_id= '$customer_ID'"; 
         $result = mysqli_query($conn, $order_query); 
         $index=0;
-       while ($row = mysqli_fetch_assoc($result)) 
-       {       
-              
-               $bookId[$index]    =     $row['book_id'];
-              
-               $qty[$index]       =     $row['quantity'];
-               $totalPrice[$index]=     $row['total_price'];
-               $index++;
-          
-       }
 
+        while ($row = mysqli_fetch_assoc($result)) {       
+            $bookId[$index]    =     $row['book_id'];
+            $qty[$index]       =     $row['quantity'];
+            $totalPrice[$index]=     $row['total_price'];
+            $index++;
+        }
 
-            $shipping= $_SESSION['shipping_address'];
-        
-            $custOrder_query= "INSERT INTO customer_order (customer_id, order_date, shipping_status, shipping_adress) 
-                            VALUES( '$customer_ID', '$order_date', 'PLACED', '$shipping')";
-                     mysqli_query($conn, $custOrder_query);
+        $shipping= $_SESSION['shipping_address'];
 
+        $custOrder_query= "INSERT INTO customer_order (customer_id, order_date, shipping_status, shipping_adress) 
+                        VALUES( '$customer_ID', '$order_date', 'PLACED', '$shipping')";
+        mysqli_query($conn, $custOrder_query);
 
-                     $customerOrder_query = "SELECT order_id FROM customer_order WHERE 
-                            customer_id= '$customer_ID' ORDER BY order_id DESC LIMIT 1";                      
-                     $result = mysqli_query($conn, $customerOrder_query);
-                     $orderrow = mysqli_fetch_assoc($result);
+        $customerOrder_query = "SELECT order_id FROM customer_order WHERE 
+            customer_id= '$customer_ID' ORDER BY order_id DESC LIMIT 1";                      
+        $result = mysqli_query($conn, $customerOrder_query);
+        $orderrow = mysqli_fetch_assoc($result);
 
-                   // $lastOrder= "SELECT * FROM customer_order WHERE id=(SELECT max(id) FROM customer_order)";
-                    
-                   
-                    $orderid=$orderrow['order_id'];
-                    
+        // $lastOrder= "SELECT * FROM customer_order WHERE id=(SELECT max(id) FROM customer_order)";
+                        
+        $orderid=$orderrow['order_id'];
 
         for( $i=0; $i<count($bookId); $i++){
             $b=$bookId[$i];
@@ -100,64 +88,63 @@ require_once("../templates/header.php");
             $p=$totalPrice[$i];
             $itemOrder_query= "INSERT INTO order_items (book_id, order_id, quantity,total_price) 
                             VALUES( '$b','$orderid', '$q', '$p')";
-                     mysqli_query($conn, $itemOrder_query);
+            mysqli_query($conn, $itemOrder_query);
         }
 
-        $delet_query= "DELETE FROM cart WHERE customer_id= '$customer_ID'";
-        mysqli_query($conn, $delet_query);
-   
-}
- 
+        $delete_query= "DELETE FROM cart WHERE customer_id= '$customer_ID'";
+        mysqli_query($conn, $delete_query);
+    }
+
 ?>
 
 
-<div class="container" class="nav nav-pills mb-3 justify-content-center" style="margin-top: 100px; margin-bottom: 100px; margin: left 100px; width:40%;">
-	
-        <form action="payment_process.php" method="post" id="login-form">
-        <p style="text-align: center"> <b> Please Enter Card details </b> <p>
-      
-
-
+<div class="container" class="nav nav-pills mb-3 justify-content-center" 
+    style="margin-top: 100px; margin-bottom: 100px; margin: left 100px; width:40%;">
+    <form action="payment_process.php" method="post" id="login-form">
+        <p style="text-align: center"> 
+            <strong> Please Enter Card details </strong>
+        </p>
         <div class="form-group row">
-                <label for="card_type" class="col-sm-3 col-form-label signup-label">Type</label>
-                <div class="col-sm-9">
+            <label for="card_type" class="col-sm-3 col-form-label signup-label">Type</label>
+            <div class="col-sm-9">
                 <select class="form-control" name="card_type" required>
-                        <option value="VISA">VISA</option>
-                        <option value="MasterCard">MasterCard</option>
-                        <option value="American Express">American Express</option>
-                    </select>  
-                 </div>
-         </div>
+                    <option value="VISA">VISA</option>
+                    <option value="MasterCard">MasterCard</option>
+                    <option value="American Express">American Express</option>
+                </select>  
+            </div>
+        </div>
         <div class="form-group row"> 
             <label for="card_number" class="col-sm-3 col-form-label signup-label">Number</label>
-             <div class="col-sm-9">
-              	<input type="text"  class="form-control" name="card_number" required>
+            <div class="col-sm-9">
+                <input type="text"  class="form-control" name="card_number" required>
             </div>
         </div>
         <div class="form-group row">
             <label for="card_PID" class="col-sm-3 col-form-label signup-label">PID</label>
             <div class="col-sm-9">
-              	<input type="number" class="form-control" name="card_PID" required>
+                <input type="number" class="form-control" name="card_PID" required>
             </div>
         </div>
-
         <div class="form-group row">
             <label for="date" class="col-sm-3 col-form-label signup-label">Date</label>
             <div class="col-sm-9">
-              	<input type="date"  class="form-control" name="date" required>
+                <input type="date"  class="form-control" name="date" required>
             </div>
         </div>
         <div class="form-group row">
             <label for="card_owner" class="col-sm-3 col-form-label signup-label">Name</label>
             <div class="col-sm-9">
-              	<input type="text" class="form-control" name="card_owner" required>
+                <input type="text" class="form-control" name="card_owner" required>
             </div>
-        </div>  <div class="text-center">
-              	<button type="reset" class="btn btn-primary">Cancel</button>
-              	<button type="submit" style="align-items:center; "  class="btn btn-primary" name="save-order"  >Purchase</button>
         </div>
+        <div class="text-center">
+            <button type="reset" class="btn btn-primary">Cancel</button>
+            <button type="submit" style="align-items:center;" 
+                class="btn btn-primary" name="save-order">Purchase
+            </button>
         </div>
-            </div>
-		</div>
     </form>
-    <?php require_once("../templates/footer.php"); 
+</div>
+
+<?php require_once("../templates/footer.php"); ?>
